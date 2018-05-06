89c204f198171905e6762e4bff811370df235397
icourt1 <parker07252005@gmail.com>
2018-05-05 22:54:25 -0500

    Removed files

--- Vulcan/Court-Discord-Bot-master/events/ready.js
+++
@@ -1,9 +0,0 @@
-const tokens = require('../tokens.json');
-const Discord = require("discord.js");
-const client = new Discord.Client({fetchAllMembers: true});
-const log = require(`../handlers/logHandler.js`);
-
-module.exports = async client => {
-  client.user.setActivity(`${tokens.prefix}help | ${client.users.keyArray().length} Users Online`, { type: `${tokens.activity.type}`, url: `${tokens.activity.url}` });
-  log.info(`${client.users.keyArray().length} Users Online`);
-}
